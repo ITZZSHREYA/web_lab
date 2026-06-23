ec8ead6217b2665842bcda4328c43adbc93efa10
Shreya <137786933+ITZZSHREYA@users.noreply.github.com>
2026-06-23 23:55:17 +0530

    Update 12_login.php

--- 12_login.php
+++ 12_login.php
@@ -40,3 +40,8 @@ if($_SERVER["REQUEST_METHOD"]=="POST")
 </html>
 login.txt
 shreya:123
+
+##1.Create into.txt and index.php file code accordingly. in Same directory. 
+2. change the file.txt modo. 
+>chmat info.txt 777 
+3. Go to browser and execute the filename.php. 
